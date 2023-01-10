055b30039f51b1b0123309d6a91b39c8a22edb33
heroq <96118954+heroq@users.noreply.github.com>
2023-01-10 20:33:37 +0900

    링크드리스트

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,7 +1,15 @@
 public class SinglyLinkedList {
+    public class Node(){
+        private Node next;
+        private String value;
 
-}
+        Node(String value){
+            this.value = value;
+            next = null;
+        }
 
-public class Node(){
-
+        public String toString(){
+            return String.valueof(this.value);
+        }
+    }
 }
